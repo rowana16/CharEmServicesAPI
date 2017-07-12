f170b6786beba75568d71e5620a0944644d68c8a
rowana16 <aaron.rowan@gmail.com>
2017-07-12 14:26:27 -0400

    Commit

---
+++ CharEmServicesNet2/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CharEmServicesNet2.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ CharEmServicesNet2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CharEmServicesNet2.WebApiApplication" Language="C#" %>
